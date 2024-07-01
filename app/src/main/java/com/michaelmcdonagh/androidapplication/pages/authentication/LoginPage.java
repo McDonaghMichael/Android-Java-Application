791eb5ac9c71560ea0f717e26e95cf225c970af8
McDonaghMichael <G00421944@atu.ie>
2024-07-01 22:03:18 +0100

    Mini update

--- app/src/main/java/com/michaelmcdonagh/androidapplication/pages/authentication/LoginPage.java
+++ app/src/main/java/com/michaelmcdonagh/androidapplication/pages/authentication/LoginPage.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,6 +20,16 @@ public class LoginPage extends AppCompatActivity {
         setContentView(R.layout.loginpage);
 
         Button loginButton = findViewById(R.id.login_button);
+
+        TextView textView = findViewById(R.id.register_link);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginPage.this, RegisterPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/michaelmcdonagh/androidapplication/pages/authentication/RegisterPage.java
+++ app/src/main/java/com/michaelmcdonagh/androidapplication/pages/authentication/RegisterPage.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.michaelmcdonagh.androidapplication.MainActivity;
 import com.michaelmcdonagh.androidapplication.R;
 import com.michaelmcdonagh.androidapplication.pages.home.Homepage;
 
@@ -18,6 +20,15 @@ public class RegisterPage extends AppCompatActivity {
 
         setContentView(R.layout.registerpage);
 
+        TextView textView = findViewById(R.id.login_link);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterPage.this, LoginPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
 
     }
